ab9da3dea6e76307e445e5638a48876d0caa755f
Young K. Park <youngkeunpark@gmail.com>
2012-06-19 09:32:50 -0700

    function added

--- placeholder.js
+++ placeholder.js
@@ -13,8 +13,7 @@
             var $originalColor = $(this).css("color");
 
             if($(this).attr("placeholder") && $(this).val().length == 0){
-                $(this).val($(this).attr("placeholder")).css({'font-style':$.fn.placeholder.defaults.fontStyle, 
-                    'color':$.fn.placeholder.defaults.watermarkColor}).data('init-placeholder', 'true');
+                setPlaceholder($(this));
             }
 
             $(this).on("focus", function(e){
@@ -22,7 +21,18 @@
                     $(this).val("").css({'font-style':'normal', 'color':$originalColor}).removeData("init-placeholder");
                 }
             });
+
+            $(this).on("change", function(e){
+                if($(this).val().length == 0){
+                    setPlaceholder($(this));
+                }
+            });
         });
+    
+        function setPlaceholder(elem){
+            elem.val(elem.attr("placeholder")).css({'font-style':$.fn.placeholder.defaults.fontStyle, 
+                    'color':$.fn.placeholder.defaults.watermarkColor}).data('init-placeholder', 'true');
+        }
 	};
 
     $.fn.placeholder.defaults = {
